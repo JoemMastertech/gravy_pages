8190ba47b80b13eaffc4260557f08fb4d10fa517
JoemMastertech <mastertechnologyjoel@gmail.com>
2026-03-09 18:11:23 -0600

    Deploy automático: Mon Mar  9 18:11:23     2026

--- assets/LoginModal-DHcA1oRQ.js
+++ assets/LoginModal-DHcA1oRQ.js
@@ -1,4 +1,4 @@
-import n from"./AuthService-CSunz10s.js";import"./index-BK4cBDDl.js";class u{constructor(){this.container=document.createElement("div"),this.container.id="auth-modal-container",this.container.className="login-modal-overlay hidden",this.render(),this.bindEvents(),document.body.appendChild(this.container)}render(){this.container.innerHTML=`
+import n from"./AuthService-Cc3bukR3.js";import"./index-Cl-K2mqH.js";class u{constructor(){this.container=document.createElement("div"),this.container.id="auth-modal-container",this.container.className="login-modal-overlay hidden",this.render(),this.bindEvents(),document.body.appendChild(this.container)}render(){this.container.innerHTML=`
       <div class="login-modal-card">
         <button class="close-btn">×</button>
         
