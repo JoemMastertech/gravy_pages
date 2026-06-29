82f340a8b6a59b4abd2cc5c4b292729890005ca0
Joel Montesinos <joel@example.com>
2026-06-29 17:47:17 -0600

    Deploy automático: Mon Jun 29 17:47:17     2026

--- assets/index-BdOui7QC.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-Byu2NBkT.js","./index-C239QoHQ.js","./index-CorqPrMk.css"])))=>i.map(i=>d[i]);
-import{aK as p,_ as r}from"./index-C239QoHQ.js";const o=p("App",{web:()=>r(()=>import("./web-Byu2NBkT.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
---
+++ assets/index-C1MGeNyW.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CuS1u8hI.js","./index-BdLZ7wrC.js","./index-CorqPrMk.css"])))=>i.map(i=>d[i]);
+import{aK as p,_ as r}from"./index-BdLZ7wrC.js";const o=p("App",{web:()=>r(()=>import("./web-CuS1u8hI.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
